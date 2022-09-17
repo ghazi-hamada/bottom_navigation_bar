807bde55812a17aa43a7f63e4b86875d8527ef75
ghazi <ghazihamada7@gmail.com>
2022-09-17 20:14:58 +0300

    bottom_navigation_bar

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bottom_navigation_bar/test.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: test(),
+    );
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class test extends StatefulWidget {
+  const test({Key? key}) : super(key: key);
+
+  @override
+  State<test> createState() => _testState();
+}
+
+class _testState extends State<test> {
+  int _currentindex = 0;
+  List<Widget> body = const [
+    Icon(Icons.home),
+    Icon(Icons.menu),
+    Icon(Icons.person),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: body[_currentindex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentindex,
+        onTap: (int value) {
+          setState(() {
+            _currentindex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Menu', icon: Icon(Icons.menu)),
+          BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
